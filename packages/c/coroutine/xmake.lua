340c4f325e77fe86dad5316c04cf2a71a4f92a4a
RunThem <iccy.fun@outlook.com>
2023-02-21 23:08:42 +0800

    add https://github.com/cloudwu/coroutine

---
+++ packages/c/coroutine/xmake.lua
@@ -0,0 +1,21 @@
+package("coroutine")
+    set_homepage("https://github.com/cloudwu/coroutine")
+    set_description("A asymmetric coroutine library for C.")
+    set_license("MIT")
+
+    add_urls("https://github.com/cloudwu/coroutine.git")
+
+    on_install(function (package)
+        io.writefile("xmake.lua", [[
+            add_rules("mode.release", "mode.debug")
+            target("coroutine")
+                set_kind("$(kind)")
+                add_files("coroutine.c")
+                add_headerfiles("coroutine.h")
+        ]])
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("coroutine_open", {includes = "coroutine.h"}))
+    end)
