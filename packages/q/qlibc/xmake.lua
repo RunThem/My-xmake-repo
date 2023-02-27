09b36be596b5b3ec7d4984ac79e166cb8d14c8bf
RunThem <iccy.fun@outlook.com>
2023-02-27 22:51:30 +0800

    fix qlibc unknown type name `ssize_t`

--- packages/q/qlibc/xmake.lua
+++ packages/q/qlibc/xmake.lua
@@ -6,6 +6,7 @@ package("qlibc")
              "https://github.com/wolkykim/qlibc.git")
     add_versions("v2.4.8", "e4cb1d40190e5fdd517d9e7ecdffb5ca728d80ff3d5e8308f4d1ba3b0162d433")
 
+    add_patches("v2.4.8", path.join(os.scriptdir(), "patches", "ssize_t.patch"))
     on_install(function (package)
         local configs = { "--without-openssl", "--without-mysql" }
         import("package.tools.autoconf").install(package, configs)
