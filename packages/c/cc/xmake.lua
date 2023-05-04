2ead7a6c17eb4c02899ff530379d9487a984e9f0
RunThem <iccy.fun@outlook.com>
2023-05-05 03:40:50 +0800

    fix cc version

--- packages/c/cc/xmake.lua
+++ packages/c/cc/xmake.lua
@@ -2,7 +2,9 @@ package("cc")
     set_homepage("https://github.com/JacksonAllan/CC")
     set_description("A small, usability-oriented generic container library.")
 
-    add_urls("https://github.com/JacksonAllan/CC.git")
+    add_urls("https://github.com/JacksonAllan/CC/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/JacksonAllan/CC.git")
+    add_versions("v1.0.3", "5192aae6df0c40f4990b6721ad82e8049f560c6189adb59651c2fb366796a4dc")
 
     on_install(function (package)
         local configs = {}
@@ -16,5 +18,5 @@ package("cc")
     end)
 
     -- on_test(function (package)
-    --     assert(package:has_cfuncs("init", {includes = "cc.h"}))
+    --     assert(package:has_cfuncs("foo", {includes = "foo.h"}))
     -- end)
